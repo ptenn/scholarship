caeeac2b272bfa03a64f66c5d397415a4c738971
Philip Tenn <ptenn@users.noreply.github.com>
2015-04-14 23:27:11 -0400

    Initial mini proof-of-concept:   * Set up logging (slf4j, log4j2)   * Include dependencies in Maven POM file for building a Spring/Hibernate application.   * Set up Spring Web MVC and a Hello World Controller.

---
+++ src/main/java/com/philiptenn/scholarship/HelloController.java
@@ -0,0 +1,20 @@
+package com.philiptenn.scholarship;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author <a href="mailto:ptenn@users.noreply.github.com">Philip Tenn</a>
+ */
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("message", "Hello Spring MVC Framework!");
+        return "hello";
+    }
+}
